4f34f67bd6a82b2072cf42b9177f0f686882fbff
hudesdev <hudes0112@gmail.com>
2023-05-28 23:13:45 +0300

    fix: some bugs fixed

--- back/src/lib.rs
+++ back/src/lib.rs
@@ -2,12 +2,11 @@ use borsh::{BorshDeserialize, BorshSerialize};
 
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
-    system_instruction::{create_account, transfer},
-    program::{invoke, invoke_signed},
     entrypoint,
     entrypoint::ProgramResult,
-    msg,
-    pubkey::Pubkey
+    program::{invoke, invoke_signed},
+    pubkey::Pubkey,
+    system_instruction::{create_account, transfer},
 };
 
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
@@ -15,7 +14,7 @@ struct Product {
     owner: Pubkey,
     store: Pubkey,
     name: String,
-    price: u64
+    price: u64,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
@@ -24,7 +23,6 @@ struct Store {
     owner: Pubkey,
 }
 
-
 #[derive(BorshSerialize, BorshDeserialize)]
 enum Command {
     Buy,
@@ -32,9 +30,8 @@ enum Command {
     AddToStore(Product, u64),
     DeleteFromStore,
     Close,
-    Wipe
+    Wipe,
 }
-
 
 fn buy(accounts: &[AccountInfo]) {
     let mut accounts_iter = accounts.iter();
@@ -52,59 +49,78 @@ fn buy(accounts: &[AccountInfo]) {
     invoke(&instruction, accounts).unwrap();
 
     product.owner = *from;
-    product.serialize(&mut *product_info.try_borrow_mut_data().unwrap()).unwrap();
+    product
+        .serialize(&mut *product_info.try_borrow_mut_data().unwrap())
+        .unwrap();
 }
 
-fn make_store(program_id: &Pubkey, accounts: &[AccountInfo], store: &Store, rent_exemption: u64)  {
+fn make_store(program_id: &Pubkey, accounts: &[AccountInfo], store: &Store, rent_exemption: u64) {
     let usr = &store.owner;
 
-    let (account_pubkey, bump) = Pubkey::find_program_address(&[store.name_of_store.as_bytes()], program_id);
-    
-    let instruction = create_account (
-        &usr,
-        &account_pubkey,
-        rent_exemption, 
-        50 + 32,
-        program_id
-    );
-    
-    invoke_signed(&instruction, accounts, &[&[store.name_of_store.as_bytes(), &[bump]]]).unwrap();
-    
+    let (account_pubkey, bump) =
+        Pubkey::find_program_address(&[store.name_of_store.as_bytes()], program_id);
+
+    let instruction = create_account(&usr, &account_pubkey, rent_exemption, 50 + 32, program_id);
+
+    invoke_signed(
+        &instruction,
+        accounts,
+        &[&[store.name_of_store.as_bytes(), &[bump]]],
+    )
+    .unwrap();
+
     let account = next_account_info(&mut accounts.into_iter()).unwrap();
-    store.serialize(&mut *account.try_borrow_mut_data().unwrap()).unwrap();
+    store
+        .serialize(&mut *account.try_borrow_mut_data().unwrap())
+        .unwrap();
 }
 
-fn add_to_store(program_id: &Pubkey, accounts: &[AccountInfo], product: &Product, rent_exemption: u64)  {
+fn add_to_store(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    product: &Product,
+    rent_exemption: u64,
+) {
     let accounts_iter = &mut accounts.into_iter();
 
     let product_info = next_account_info(accounts_iter).unwrap();
     let store_info = next_account_info(accounts_iter).unwrap();
     let usr_info = next_account_info(accounts_iter).unwrap();
 
-    let store = Store::deserialize(&mut store_info.data.borrow_mut().as_ref()).expect("Cannot deserialize store account");
+    let store = Store::deserialize(&mut store_info.data.borrow_mut().as_ref())
+        .expect("Cannot deserialize store account");
     assert!(product.name.len() <= 50);
-    assert_eq!(&product.owner, store_info.key);
     assert_eq!(&store.owner, usr_info.key);
+    assert!(usr_info.is_signer);
 
     let store_pubkey = &product.owner;
 
-    let (_, bump) = Pubkey::find_program_address(&[&store_pubkey.to_bytes(), product.name.as_bytes()], program_id);
-    
-    let instruction = create_account (
+    let (_, bump) = Pubkey::find_program_address(
+        &[&store_pubkey.to_bytes(), product.name.as_bytes()],
+        program_id,
+    );
+
+    let instruction = create_account(
         &usr_info.key,
         &product_info.key,
-        rent_exemption, 
-        32 + 32 + 50 + 8 + 8, 
-        program_id
+        rent_exemption,
+        32 + 32 + 50 + 8 + 8,
+        program_id,
     );
-    
-    invoke_signed(&instruction, accounts, &[&[&store_pubkey.to_bytes(), product.name.as_bytes(), &[bump]]]).unwrap();
-    
-    product.serialize(&mut *product_info.try_borrow_mut_data().unwrap()).unwrap();
+
+    invoke_signed(
+        &instruction,
+        accounts,
+        &[&[&store_pubkey.to_bytes(), product.name.as_bytes(), &[bump]]],
+    )
+    .unwrap();
+
+    product
+        .serialize(&mut *product_info.try_borrow_mut_data().unwrap())
+        .unwrap();
 }
 
-
-fn delete_from_store(accounts: &[AccountInfo])  {
+fn delete_from_store(accounts: &[AccountInfo]) {
     let accounts_iter = &mut accounts.iter();
     let product_info = next_account_info(accounts_iter).unwrap();
     let store_info = next_account_info(accounts_iter).unwrap();
@@ -116,8 +132,10 @@ fn delete_from_store(accounts: &[AccountInfo])  {
     assert_eq!(&product.owner, store_info.key);
     assert_eq!(&store.owner, owner_info.key);
 
-
-    **owner_info.lamports.borrow_mut() = owner_info.lamports().checked_add(product_info.lamports()).unwrap();
+    **owner_info.lamports.borrow_mut() = owner_info
+        .lamports()
+        .checked_add(product_info.lamports())
+        .unwrap();
     **product_info.lamports.borrow_mut() = 0;
 }
 
@@ -129,8 +147,10 @@ fn close(accounts: &[AccountInfo]) {
     let store = Store::deserialize(&mut store_info.data.borrow_mut().as_ref()).unwrap();
     assert_eq!(&store.owner, owner_info.key);
 
-
-    **owner_info.lamports.borrow_mut() = owner_info.lamports().checked_add(store_info.lamports()).unwrap();
+    **owner_info.lamports.borrow_mut() = owner_info
+        .lamports()
+        .checked_add(store_info.lamports())
+        .unwrap();
     **store_info.lamports.borrow_mut() = 0;
 }
 
@@ -139,38 +159,33 @@ fn wipe(accounts: &[AccountInfo]) {
     let account_info = next_account_info(accounts_iter).unwrap();
     let owner_info = next_account_info(accounts_iter).unwrap();
 
-    **owner_info.lamports.borrow_mut() = owner_info.lamports().checked_add(account_info.lamports()).unwrap();
+    **owner_info.lamports.borrow_mut() = owner_info
+        .lamports()
+        .checked_add(account_info.lamports())
+        .unwrap();
     **account_info.lamports.borrow_mut() = 0;
 }
 
-
 entrypoint!(process_instruction);
-pub fn process_instruction (
-    program_id: &Pubkey, 
-    accounts: &[AccountInfo], 
-    instruction_data: &[u8]
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!("OU MY clock");
-
     let command = Command::try_from_slice(instruction_data).expect("Can't Deserialize command");
 
     match command {
-        Command::MakeStore(store, rent_exemption) => make_store(&program_id, accounts, &store, rent_exemption),
+        Command::MakeStore(store, rent_exemption) => {
+            make_store(&program_id, accounts, &store, rent_exemption)
+        }
         Command::Buy => buy(accounts),
-        Command::AddToStore(product, rent_exemption) => add_to_store(&program_id, accounts, &product, rent_exemption),
+        Command::AddToStore(product, rent_exemption) => {
+            add_to_store(&program_id, accounts, &product, rent_exemption)
+        }
         Command::DeleteFromStore => delete_from_store(accounts),
         Command::Close => close(accounts),
-        Command::Wipe => wipe(accounts)
+        Command::Wipe => wipe(accounts),
     };
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
 }
--- front/src/main.rs
+++ front/src/main.rs
@@ -1,36 +1,28 @@
-use std::{str::FromStr, borrow::Borrow};
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_account_decoder::UiAccountEncoding;
 use solana_client::{
-    rpc_client::RpcClient, 
-    rpc_filter::{
-        RpcFilterType, 
-        Memcmp, 
-        MemcmpEncodedBytes
-    }, 
-    rpc_config::{
-        RpcProgramAccountsConfig, 
-        RpcAccountInfoConfig
-    }
+    rpc_client::RpcClient,
+    rpc_config::{RpcAccountInfoConfig, RpcProgramAccountsConfig},
+    rpc_filter::{Memcmp, MemcmpEncodedBytes, RpcFilterType},
 };
-use borsh::{BorshDeserialize, BorshSerialize};
 use solana_sdk::{
+    account::Account,
+    instruction::{AccountMeta, Instruction},
     message::Message,
-    instruction::{Instruction, AccountMeta},
-    signer::{keypair::read_keypair_file, Signer},
     pubkey,
     pubkey::Pubkey,
-    transaction::Transaction, signature::Keypair, account::Account
+    signature::Keypair,
+    signer::{keypair::read_keypair_file, Signer},
+    transaction::Transaction,
 };
-
-
-
+use std::{borrow::Borrow, str::FromStr};
 
 #[derive(BorshDeserialize, BorshSerialize, Debug)]
 struct Product {
     owner: Pubkey,
     store: Pubkey,
     name: String,
-    price: u64
+    price: u64,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
@@ -39,7 +31,6 @@ struct Store {
     owner: Pubkey,
 }
 
-
 #[derive(BorshSerialize, BorshDeserialize)]
 enum Command {
     Buy,
@@ -47,35 +38,40 @@ enum Command {
     AddToStore(Product, u64),
     DeleteFromStore,
     Close,
-    Wipe
+    Wipe,
 }
 
-
-fn get_prgram_accounts(rpc_client: &RpcClient, program_id: &Pubkey, memcmps: Vec<RpcFilterType>) -> Vec<(Pubkey, Account)>{
-
+fn get_prgram_accounts(
+    rpc_client: &RpcClient,
+    program_id: &Pubkey,
+    memcmps: Vec<RpcFilterType>,
+) -> Vec<(Pubkey, Account)> {
     let config = RpcProgramAccountsConfig {
         filters: Some(memcmps),
         account_config: RpcAccountInfoConfig {
             encoding: Some(UiAccountEncoding::Base64),
             data_slice: None,
-            commitment: None
+            commitment: None,
         },
         with_context: Some(false),
     };
 
-    rpc_client.get_program_accounts_with_config(
-        &program_id,
-        config,
-    ).unwrap()
+    rpc_client
+        .get_program_accounts_with_config(&program_id, config)
+        .unwrap()
 }
-
 
 fn buy(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: &Vec<String>) {
     let store_pubkey = Pubkey::from_str(&args[2]).unwrap();
     let product = String::from(&args[3]);
-    let (product_pubkey, _) = Pubkey::find_program_address(&[&store_pubkey.to_bytes(), product.as_bytes()], program_id);
-    
-    
+    let (product_pubkey, _) =
+        Pubkey::find_program_address(&[&store_pubkey.to_bytes(), product.as_bytes()], program_id);
+
+    let store_account = rpc_client.get_account(&store_pubkey).unwrap();
+
+    let store = Store::deserialize(&mut store_account.data.as_ref()).unwrap();
+    let store_owner = store.owner;
+
     let command = Command::Buy;
     let instruction = Instruction::new_with_borsh(
         *program_id,
@@ -84,9 +80,10 @@ fn buy(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: &Vec<St
             AccountMeta::new(usr.pubkey(), true),
             AccountMeta::new(product_pubkey, false),
             AccountMeta::new_readonly(store_pubkey, false),
-            AccountMeta::new_readonly(pubkey!("11111111111111111111111111111111"), false)
-        ]);
-
+            AccountMeta::new(store_owner, false),
+            AccountMeta::new_readonly(pubkey!("11111111111111111111111111111111"), false),
+        ],
+    );
 
     let message = Message::new(&[instruction], None);
     let tx = Transaction::new(&[usr], message, rpc_client.get_latest_blockhash().unwrap());
@@ -102,7 +99,10 @@ fn get_store(rpc_client: &RpcClient, args: &Vec<String>) {
     let store = Store::deserialize(&mut store_account.data.as_ref()).unwrap();
     let program_id = store_account.owner;
 
-    println!("Name of store: {}\nOwner: {}\n", store.name_of_store, store.owner);
+    println!(
+        "Name of store: {}\nOwner: {}\n",
+        store.name_of_store, store.owner
+    );
 
     let memcmp = RpcFilterType::Memcmp(Memcmp {
         offset: 0,
@@ -111,8 +111,6 @@ fn get_store(rpc_client: &RpcClient, args: &Vec<String>) {
     });
 
     let accounts = get_prgram_accounts(rpc_client, &program_id, vec![memcmp]);
-
-
 
     println!("Products:");
     for (_, account) in accounts.iter() {
@@ -134,17 +132,18 @@ fn get_product(rpc_client: &RpcClient, args: &Vec<String>) {
 fn make_store(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: &Vec<String>) {
     let name_of_store = String::from(&args[2]);
     assert!(name_of_store.len() <= 50);
-    let store =  Store{
-        name_of_store, 
-        owner: usr.pubkey(), 
+    let store = Store {
+        name_of_store,
+        owner: usr.pubkey(),
     };
 
-    let rent_exemption = rpc_client.get_minimum_balance_for_rent_exemption(50 + 32).unwrap();
+    let rent_exemption = rpc_client
+        .get_minimum_balance_for_rent_exemption(50 + 32)
+        .unwrap();
 
-
-    let (account_pubkey, _) = Pubkey::find_program_address(&[store.name_of_store.as_bytes()], program_id);
+    let (account_pubkey, _) =
+        Pubkey::find_program_address(&[store.name_of_store.as_bytes()], program_id);
     let command = Command::MakeStore(store, rent_exemption);
-    
 
     let instruction = Instruction::new_with_borsh(
         *program_id,
@@ -153,10 +152,9 @@ fn make_store(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: 
             AccountMeta::new(account_pubkey, false),
             AccountMeta::new(usr.pubkey(), true),
             AccountMeta::new_readonly(pubkey!("11111111111111111111111111111111"), false),
-        ] 
+        ],
     );
 
-    
     let message = Message::new(&[instruction], None);
     let tx = Transaction::new(&[usr], message, rpc_client.get_latest_blockhash().unwrap());
     send(&rpc_client, &tx);
@@ -164,22 +162,26 @@ fn make_store(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: 
 }
 
 fn add_to_store(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: &Vec<String>) {
-
     let name_of_store = String::from(&args[2]);
     let name_of_product = String::from(&args[3]);
     assert!(name_of_product.len() <= 50);
     let (store_pubkey, _) = Pubkey::find_program_address(&[name_of_store.as_bytes()], &program_id);
-    let (account_pubkey, _) = Pubkey::find_program_address(&[&store_pubkey.to_bytes(), name_of_product.as_bytes()], program_id);
+    let (account_pubkey, _) = Pubkey::find_program_address(
+        &[&store_pubkey.to_bytes(), name_of_product.as_bytes()],
+        program_id,
+    );
 
     let product = Product {
         owner: store_pubkey,
-        store: store_pubkey, 
+        store: store_pubkey,
         name: name_of_product,
-        price: args[4].parse().unwrap()
+        price: args[4].parse().unwrap(),
     };
 
-    let rent_exemption = rpc_client.get_minimum_balance_for_rent_exemption(32 + 32 + 50 + 8 + 8).unwrap();
-    
+    let rent_exemption = rpc_client
+        .get_minimum_balance_for_rent_exemption(32 + 32 + 50 + 8 + 8)
+        .unwrap();
+
     let command = Command::AddToStore(product, rent_exemption);
 
     let instruction = Instruction::new_with_borsh(
@@ -190,35 +192,40 @@ fn add_to_store(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args
             AccountMeta::new_readonly(store_pubkey, false),
             AccountMeta::new(usr.pubkey(), true),
             AccountMeta::new_readonly(pubkey!("11111111111111111111111111111111"), false),
-        ] 
+        ],
     );
 
-    
     let message = Message::new(&[instruction], None);
     let tx = Transaction::new(&[usr], message, rpc_client.get_latest_blockhash().unwrap());
     send(&rpc_client, &tx);
     println!("Successfully added: {}", account_pubkey);
 }
 
-fn delete_from_store(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: &Vec<String>) {
-
+fn delete_from_store(
+    rpc_client: &RpcClient,
+    usr: &Keypair,
+    program_id: &Pubkey,
+    args: &Vec<String>,
+) {
     let name_of_store = String::from(&args[2]);
     let name_of_product = String::from(&args[3]);
     let (store_pubkey, _) = Pubkey::find_program_address(&[name_of_store.as_bytes()], program_id);
-    let (product_pubkey, _) = Pubkey::find_program_address(&[&store_pubkey.to_bytes(), name_of_product.as_bytes()], program_id);
+    let (product_pubkey, _) = Pubkey::find_program_address(
+        &[&store_pubkey.to_bytes(), name_of_product.as_bytes()],
+        program_id,
+    );
 
     let command = Command::DeleteFromStore;
     let instruction = Instruction::new_with_borsh(
-            *program_id,
+        *program_id,
         &command,
         vec![
             AccountMeta::new(product_pubkey, false),
             AccountMeta::new(store_pubkey, false),
-            AccountMeta::new(usr.pubkey(), true)
-        ] 
+            AccountMeta::new(usr.pubkey(), true),
+        ],
     );
 
-    
     let message = Message::new(&[instruction], None);
     let tx = Transaction::new(&[usr], message, rpc_client.get_latest_blockhash().unwrap());
     send(&rpc_client, &tx);
@@ -226,30 +233,29 @@ fn delete_from_store(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey,
 }
 
 fn close(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: &Vec<String>) {
-
     let store_pubkey = Pubkey::from_str(&args[2]).unwrap();
     let command = Command::Close;
-    
+
     let memcmp_owner = RpcFilterType::Memcmp(Memcmp {
         offset: 0,
         bytes: MemcmpEncodedBytes::Base64(store_pubkey.to_string()),
         encoding: None,
     });
-    
+
     let accounts = get_prgram_accounts(rpc_client, program_id, vec![memcmp_owner]);
 
     for (product_pubkey, _) in accounts.iter() {
         let command = Command::DeleteFromStore;
         let instruction = Instruction::new_with_borsh(
-                *program_id,
+            *program_id,
             &command,
             vec![
                 AccountMeta::new(*product_pubkey, false),
                 AccountMeta::new(store_pubkey, false),
-                AccountMeta::new(usr.pubkey(), true)
-            ] 
+                AccountMeta::new(usr.pubkey(), true),
+            ],
         );
-        
+
         let message = Message::new(&[instruction], None);
         let tx = Transaction::new(&[usr], message, rpc_client.get_latest_blockhash().unwrap());
         send(&rpc_client, &tx);
@@ -260,16 +266,15 @@ fn close(rpc_client: &RpcClient, usr: &Keypair, program_id: &Pubkey, args: &Vec<
         &command,
         vec![
             AccountMeta::new(store_pubkey, false),
-            AccountMeta::new(usr.pubkey(), true)
-        ] 
+            AccountMeta::new(usr.pubkey(), true),
+        ],
     );
-    
+
     let message = Message::new(&[instruction], None);
     let tx = Transaction::new(&[usr], message, rpc_client.get_latest_blockhash().unwrap());
     send(&rpc_client, &tx);
     println!("Successfully deleted: {}", store_pubkey);
 }
-
 
 fn send(rpc_client: &RpcClient, tx: &Transaction) {
     let _result = rpc_client.send_and_confirm_transaction(&tx).unwrap();
@@ -277,20 +282,19 @@ fn send(rpc_client: &RpcClient, tx: &Transaction) {
 }
 
 fn wipe(rpc_client: &RpcClient, program_id: &Pubkey, usr: &Keypair) {
-    
     let accounts = get_prgram_accounts(rpc_client, program_id, vec![]);
 
     for (account_pubkey, _) in accounts.iter() {
         let command = Command::Wipe;
         let instruction = Instruction::new_with_borsh(
-                *program_id,
+            *program_id,
             &command,
             vec![
                 AccountMeta::new(*account_pubkey, false),
-                AccountMeta::new(usr.pubkey(), true)
-            ] 
+                AccountMeta::new(usr.pubkey(), true),
+            ],
         );
-        
+
         let message = Message::new(&[instruction], None);
         let tx = Transaction::new(&[usr], message, rpc_client.get_latest_blockhash().unwrap());
         send(&rpc_client, &tx);
@@ -298,25 +302,21 @@ fn wipe(rpc_client: &RpcClient, program_id: &Pubkey, usr: &Keypair) {
     println!("Wiped");
 }
 
-
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     if args.len() < 2 {
-        eprintln!(
-            "usage: {} <command>",
-            args[0]
-        );
+        eprintln!("usage: {} <command>", args[0]);
         std::process::exit(-1);
     }
 
     let arg = &args[1];
 
     let rpc_client = RpcClient::new("https://api.devnet.solana.com");
-    let usr = read_keypair_file("/home/gohnny/.config/solana/id.json").unwrap();
-    let program = pubkey!("J9cFnPQkWmLsXWEoARkdB9QJQksnMD4Bk76ptppLpPZU");
-    
+    let usr = read_keypair_file("/home/gohnny/.config/solana/dev.json").unwrap();
+    let program = pubkey!("74yFYZY2m29ViLuFnVkmMAmhRmjKgREVcbqhtSTG3bJY");
+
     match arg.as_str() {
-        "get-store" => get_store(&rpc_client, &args), 
+        "get-store" => get_store(&rpc_client, &args),
         "get-product" => get_product(&rpc_client, &args),
         "make-store" => make_store(&rpc_client, &usr, &program, &args),
         "add-to-store" => add_to_store(&rpc_client, &usr, &program, &args),
@@ -324,6 +324,6 @@ fn main() {
         "buy" => buy(&rpc_client, &usr, &program, &args),
         "close" => close(&rpc_client, &usr, &program, &args),
         "wipe" => wipe(&rpc_client, &program, &usr),
-        _ => println!("Wrong command")
+        _ => println!("Wrong command"),
     }
 }
